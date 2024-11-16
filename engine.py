4b20ba73fd83393022a9b80d38b1de848d852343
Arbab <arbabhusain200@gmail.com>
2024-11-16 12:38:12 -0500

    renamed to engine

--- engine.py
+++ engine.py
@@ -1,42 +1,54 @@
-#piece direction
 
-pieces = {
-    "white_pawns":0xFF00,
-    "white_king":0x10,
-    "white_queen":0x8,
-    "white_rook":0x81,
-    "white_knight": 0x42,
-    "white_bishop": 0x24,
-    "black_pawns":0xFF000000000000,
-    "black_king": 0x1000000000000000,  
-    "black_queen": 0x0800000000000000,  
-    "black_rook": 0x8100000000000000,  
-    "black_knight": 0x4200000000000000,  
-    "black_bishop": 0x2400000000000000,
-}
+A1, H1 = 0, 7
+A8, H8 = 56, 63
+
+#initialize starting board
+class State:
+    def __init__(self):
+        self.pieces = {
+            "white_pawns":0xFF00,
+            "white_king":0x10,
+            "white_queen":0x8,
+            "white_rook":0x81,
+            "white_knight": 0x42,
+            "white_bishop": 0x24,
+            "black_pawns":0xFF000000000000,
+            "black_king": 0x1000000000000000,  
+            "black_queen": 0x0800000000000000,  
+            "black_rook": 0x8100000000000000,  
+            "black_knight": 0x4200000000000000,  
+            "black_bishop": 0x2400000000000000,
+        }
        
-board_state = 0x0 
-for piece, position in pieces.items():
-    board_state = board_state | position
-
-print(f"{board_state:064b}")
-
-def getBoard():
-    board = []
-    for i in range(63, -1, -1):
-        board.append(int(bool(board_state & (1 << i))))
-    return board
-
-def showBoard():
-    board = getBoard()
-    for i in range(64):
-        if i % 8 == 0:
-            print("\n")
-            print(board[i], end=" ")
-        else:
-            print(board[i], end=" ")
-    print("\n")
-    
+        self.board_state = 0x0 
+        for piece, position in self.pieces.items():
+            self.board_state = self.board_state | position
         
 
-showBoard()
+        self.in_check = False
+        self.in_stalemate = False
+        self.in_mate = False
+    
+    def getBoard(self):
+        board = []
+        for i in range(63, -1, -1):
+            board.append(int(bool(self.board_state & (1 << i))))
+        return board
+    
+    def showBoard(self):
+        board = self.getBoard()
+        for i in range(64):
+            if i % 8 == 0:
+                print("\n")
+                print(board[i], end=" ")
+            else:
+                print(board[i], end=" ")
+        print("\n")
+
+        
+        
+
+
+
+
+    
