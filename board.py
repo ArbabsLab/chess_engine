03efd821f2680a8b2c120e60fb7501d2c3c120ee
Arbab Husain <arbabhusain200@gmail.com>
2024-11-21 19:41:25 -0500

    Added chess board

--- board.py
+++ board.py
@@ -1,18 +1,38 @@
 import pygame
 
-lightSquare = False
-colorLight = "white"
-colorDark = "black"
+pygame.init()
+WIDTH, HEIGHT = 640, 640
+SQUARE = WIDTH // 8
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Chessboard")
+
+
+colorLight = (255, 255, 255)
+colorDark = (0, 0, 0)
 
 def createBoard():
+    lightSquare = True
     for i in range(8):
         for j in range(8):
             if lightSquare:
                 squareColor = colorLight
             else:
                 squareColor = colorDark
+            x = j * SQUARE
+            y = i * SQUARE
+            pygame.draw.rect(screen, squareColor, (x, y, SQUARE, SQUARE))
             lightSquare = not lightSquare
+        lightSquare = not lightSquare
 
-            #find position of square
-            #draw square
-                
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run= False
+    
+    createBoard()
+    pygame.display.flip()
+
+pygame.quit()
+exit()
