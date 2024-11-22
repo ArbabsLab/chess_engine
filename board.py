3692bad1b4580e0c19274d64412c9ad41414b28b
Arbab Husain <arbabhusain200@gmail.com>
2024-11-21 19:20:12 -0500

    Working on GUI

--- board.py
+++ board.py
@@ -1,26 +1,18 @@
+import pygame
 
-def makeNewBoard():
-    board = []
+lightSquare = False
+colorLight = "white"
+colorDark = "black"
+
+def createBoard():
     for i in range(8):
-        board.append([])
         for j in range(8):
-            if i==1 or i==6:
-                board[i].append('P')
-            elif i==0 or i==7:
-                if j == 0 or j == 7:
-                    board[i].append('R')
-                elif j == 1 or j == 6:
-                    board[i].append('N')
-                elif j == 2 or j == 5:
-                    board[i].append('B')
-                elif j == 3:
-                    board[i].append('Q')
-                elif j == 4:
-                    board[i].append('K')
+            if lightSquare:
+                squareColor = colorLight
             else:
-                board[i].append('-')
-            
-    return board
+                squareColor = colorDark
+            lightSquare = not lightSquare
 
-print(makeNewBoard())
-
+            #find position of square
+            #draw square
+                
