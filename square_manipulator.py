ba272fe02c5b840660614f7bae113666401db594
Arbab Husain <arbabhusain200@gmail.com>
2024-11-26 16:24:52 -0500

    Added quare manipulator functions

---
+++ square_manipulator.py
@@ -0,0 +1,22 @@
+
+class SquareManipulator:
+    def __init__(self):
+        self.squareOccupied = [0]*16
+        self.map = [0]*64
+        self.pieceCount = 0
+
+    def AddPieceAtSquare(self, square):
+        self.squareOccupied[self.pieceCount] = square
+        self.map[square] = self.pieceCount
+        pieceCount += 1
+
+    def RemovePieceAtSquare(self, square):
+        i = self.map[square]
+        self.squareOccupied[i] = self.squareOccupied[self.pieceCount - 1]
+        self.map[self.squareOccupied[i]] = i
+        self.pieceCount -= 1
+
+    
+       
+
+    
